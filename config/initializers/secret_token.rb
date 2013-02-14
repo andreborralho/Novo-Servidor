246d9ae9437b797f75226c04b99959dbe6733c87
Miguel <miguel@ubuntu.lan>
2013-02-14 21:08:51 +0000

    config files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackOffice::Application.config.secret_token = '9740f64d6ef373e9637071b8871e04bdb7e4af3e026b40b9634c7e5e340369c6739a47e44e8febe6aa6f56d9e008144a10e8b937436485774b9d0d4f2523d1dc'
